770a27ef99cb2806d03ae77d340a2e5cf647f838
Picolinni <82936122+Picolinni@users.noreply.github.com>
2026-04-16 01:04:16 +0100

    KAS-12 Setup KashPad backend SpringBoot project;
    Initial setup of KashPad backend SpringBoot project with minimum required dependencies;

---
+++ Kashpad-Backend/src/main/java/com/gvm/Kashpad/KashpadApplication.java
@@ -0,0 +1,13 @@
+package com.gvm.Kashpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KashpadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KashpadApplication.class, args);
+	}
+
+}
---
+++ Kashpad-Backend/src/test/java/com/gvm/Kashpad/KashpadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvm.Kashpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KashpadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
